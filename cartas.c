6d41384ca15b7957a5c7c1262b40a7d65b0aa1a5
judplaza <58659463+judplaza@users.noreply.github.com>
2019-12-10 13:36:38 +0100

    Add files via upload

--- cartas.c
+++ cartas.c
@@ -1,14 +1,158 @@
 #include <stdio.h>
 #include "cartas.h"
+#include "colores.h"
+#include "azar.h"
 
-/*int main ()
+/*
+void mostrar_carta(tcarta c);
+tbaraja iniciar_baraja ();
+tbaraja mezclar_baraja (tbaraja b); 
+void mostrar_carta_delreves(tcarta c);
+
+int main ()
 {
-    
-}*/
+    int i, j;
+    tbaraja b;
 
-void mostrar_cartas(tbaraja b){
+    b = iniciar_baraja();
     
-} //funcion que muestra las cartas
-void repartir_aleatoriamente(){
     
-} // función que reparte las cartas
+    
+    for (i=0; i<b.ncartas; i++)
+    {
+        
+        if (i%CARTASXLINEA==0)
+            printf ("|");
+        mostrar_carta(b.vcartas[i]);
+        printf ("|");
+        if ((i+1)%CARTASXLINEA==0 )
+        {
+            printf ("\n");
+            for (j=0; j<CARTASXLINEA; j++)
+                printf ("+---");
+            printf ("+\n");
+        }
+        
+    }
+    printf ("\n\n");
+    
+    b= mezclar_baraja(b);
+    
+    
+    
+    printf ("\n\n"); 
+    
+    for (i=0; i<b.ncartas; i++)
+    {
+        
+        if (i%CARTASXLINEA==0)
+            printf ("|");
+        mostrar_carta_delreves(b.vcartas[i]);
+        printf ("|");
+        if ((i+1)%CARTASXLINEA==0 )
+        {
+            printf ("\n");
+            for (j=0; j<CARTASXLINEA; j++)
+                printf ("+---");
+            printf ("+\n");
+        }
+        
+    }
+    printf ("\n");
+    for (i=0; i<b.ncartas; i++)
+    {
+        
+        if (i%CARTASXLINEA==0)
+            printf ("|");
+        mostrar_carta(b.vcartas[i]);
+        printf ("|");
+        if ((i+1)%CARTASXLINEA==0 )
+        {
+            printf ("\n");
+            for (j=0; j<CARTASXLINEA; j++)
+                printf ("+---");
+            printf ("+\n");
+        }
+        
+    }
+    printf ("\n");
+ 
+}
+*/
+
+void mostrar_carta(tcarta c)
+{
+    cambiar_color_letra(0);
+    
+    switch (c.color)
+    {
+        case 1:
+            cambiar_color_fondo(1);
+            break;
+        case 2:
+            cambiar_color_fondo(2);
+            break;
+        case 3:
+            cambiar_color_fondo(3);
+            break;
+        case 4:
+            cambiar_color_fondo(4);
+            break;
+    }
+    printf (" %d ", c.num);
+    default_attributes();
+} //funcion que muestra una carta
+
+void mostrar_carta_delreves(tcarta c)
+{
+    cambiar_color_fondo(0);
+    cambiar_color_letra (2);
+    printf ("UNO"); 
+    default_attributes();
+}
+
+tbaraja iniciar_baraja ()
+{
+    int i, j, cont;
+    tbaraja b;
+    cont=0;
+    b.ncartas=76;//prueba
+    for(i=1; i<=4; i++)
+    {
+        for (j=0; j<=9; j++)
+        {
+            b.vcartas[cont].num=j;
+            b.vcartas[cont].color=i;
+            cont++;
+        }
+        
+        for (j=1; j<=9; j++)
+        {
+            b.vcartas[cont].num=j;
+            b.vcartas[cont].color=i;
+            cont++;
+        }
+          
+            
+    }
+    return b;
+} //esta funcion crea todas las cartas
+
+tbaraja mezclar_baraja (tbaraja b)
+{
+    tcarta c;
+    int pos1, i, pos2;
+    
+    for(i=0; i<b.ncartas; i++)
+    {
+        pos1 = numero_al_azar(b.ncartas);
+        pos2 = numero_al_azar(b.ncartas);
+        c = b.vcartas[pos1];
+        b.vcartas[pos1]=b.vcartas[pos2];
+        b.vcartas[pos2]=c;
+    }  
+    return b;
+    
+    
+}
+
--- cartas.h
+++ cartas.h
@@ -1,18 +1,22 @@
 
-#include "azar.h"
-#include "colores.h"
-#include "preguntas.h"
 
+/* 
+ * File:   cartas_ok.h
+ * Author: virtual
+ *
+ * Created on 25 de noviembre de 2019, 15:48
+ */
 #ifndef CARTAS_OK_H
 
 #define CARTAS_OK_H
 
 #define DIM 76 
+#define CARTASXLINEA 15
 
 
 typedef struct
 {
-    char color; // marta: valores posibles?
+    char color; // valores posibles: 0, 1, 2, 3;
     int num;
 }tcarta;
 
@@ -24,7 +28,12 @@ typedef struct
 
 //marta: faltan parámetros
 // void mostrar_cartas(); //funcion que muestra las cartas
-void mostrar_cartas(tbaraja b); //funcion que muestra las cartas
+void mostrar_carta(tcarta c); //funcion que muestra las cartas
+void mostrar_carta_delreves(tcarta c); //funcion que muestra las cartas
+
+tbaraja iniciar_baraja (); //rellena el vector inicial                JUDITH 08/12
+
+tbaraja mezclar_baraja (tbaraja b); //mezcla el vector inicial                 JUDITH 08/12
 
 //marta: si esta función reparte las cartas de un mazo entre 
 // los jugadores, debería estar en jugadores.h
