ef99f0cabcb7a8f6231a74711f42638580a5114d
l3yl4 <58658020+l3yl4@users.noreply.github.com>
2019-12-10 13:49:43 +0100

    Update UNO.c

--- UNO.c
+++ UNO.c
@@ -6,11 +6,21 @@ int main()
 {
     tUNO p;
     
+    //inicializar_azar();
     inicializar_partida(&p);
-   
-  
-    
+    /*mostrar_estado_partida(partida);
+    while(!fin_partida(partida)) 
+    {
+        realizar_jugada(&p);
+        esperar();
+        mostrar_estado_partida(partida);
+        if (!fin_partida(partida))
+            pasar_turno(&partida);
+    }
+    mostrar_ganador(partida);
+    esperar(); */
 }
+   
 
 
 
