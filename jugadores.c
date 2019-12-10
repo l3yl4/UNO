14709521a805d50c90e16d1593fcb0668348290e
l3yl4 <58658020+l3yl4@users.noreply.github.com>
2019-12-10 10:45:57 +0100

    Update jugadores.c

--- jugadores.c
+++ jugadores.c
@@ -5,8 +5,13 @@ void tirada(){
     
 } // función que determina si puedes tirar o no, en caso de que sí se pueda determina las posibilidades
 
-void mostrar_nombre(){
+void mostrar_nombre(int *njugadores, char nombre[DIM_NOMBRE]){
+/*
+    int i=0;
     
+    for(i=1; i<*njugadores; i++)
+        printf("Turno Robot%d\n", i);  //en funcion de turno 
+*/
 } //imprimir por patalla el nombre de los jugadores
 
 void preguntar_njugadores(int *njugadores)
